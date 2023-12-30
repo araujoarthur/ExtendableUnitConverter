586f13bda7e8aaf8b81e7fbc4cfff797024bac4b
Arthur Araujo <arthur.galdino.code@proton.me>
2023-12-30 17:07:03 -0300

    Delete Win64 directory

--- Win64/Debug/converters/custom_converter_example.py
+++
@@ -1,5 +0,0 @@
-def DoConversion(from_value: float) -> float: # Must the function __must__ be named DoConversion.
-    result = from_value * 3.6
-    return result
-
-# This example converts from Meters Per Second to Kilometers Per Hour.
